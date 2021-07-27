6f77000c28797682a9dcded2ace3c5d7875af2c4
Benjamin, Binghui Zhong <zhongbinghui1995@gmail.com>
2021-07-27 08:10:39 +0800

    temp save

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
 
 const Weather = () => {
+    const [weather, setWeather] = useState({})
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=hongkong&appid=26153c0a7dd7f8c70cc47753ff149b1f`)
+            console.log(response)
+            setWeather(response.data)
+        })()
+    })
+
     return (
-        <h1>Weather</h1>
+        <div>
+            <h1>Weather</h1>
+            <p>
+                
+            </p>
+        </div>
     )
 }
 

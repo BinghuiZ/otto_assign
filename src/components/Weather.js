a0d3fd5bd914c317809ac85507f87b95ae34710a
Benjamin, Binghui Zhong <zhongbinghui1995@gmail.com>
2021-07-29 13:25:55 +0800

    test to print out data

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,40 +1,50 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 
-import { Form, Input } from 'semantic-ui-react'
+import { Form, Input, Label } from 'semantic-ui-react'
 
 const Weather = () => {
     const [weather, setWeather] = useState([]);
-    const [city, setCity] = useState('Hong Kong')
+    const [typedWord, setTypedWord] = useState('Hong Kong')
 
-    const getWeather = async () => {
-        return axios.get(
-            `https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=7&units=metric&appid=ac6427222963e24140376f46e44d3bd7`
-        );
+    const getWeather = async (keyword) => {
+        let response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${keyword}&cnt=7&units=metric&appid=ac6427222963e24140376f46e44d3bd7`);
+        console.log(response)
+        if (response.status === 200) {
+            setWeather(response.data.list)
+        }
     };
 
     useEffect(() => {
-        getWeather().then((response) => {
-            console.log('getWeather');
-            console.log(response);
-            setWeather(response.data.list);
-        });
+        getWeather(typedWord)
     }, []);
+
+    const getWeatherViews = () => {
+        console.log('getWeatherViews')
+        console.log(weather)
+        return weather.map( (w) => {
+            return(
+                <div key={w.dt}>
+                    <h1>hello world</h1>
+                    <h2>{w.dt_txt}</h2>
+                    <h2>{w.main?.feels_like}</h2>
+                </div>
+            )
+        })
+    }
 
     return (
         <div>
             <h1>Weather</h1>
-            <Form>
+            <Form onSubmit={ () => { getWeather(typedWord) } }>
                 <Form.Field>
                     <Input icon='search' placeholder='seach weather by city' onChange={(e, d) => {
-                        setCity(d.value)
-                    }} />
+                        setTypedWord(d.value)
+                    }}  />   
                 </Form.Field>
             </Form>
             <div>
-                {weather.map(w => {
-                    return <div key={w.dt}><p>{w.toString()}</p></div>
-                })}
+                { getWeatherViews() }
             </div>
         </div>
     )
